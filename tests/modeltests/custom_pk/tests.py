08235e23af743b936c40caebe357ec6af7ccb9ae
Mark Smith <mark.smith@practicalpoetry.co.uk>
2012-07-08 14:06:35 +0200

    Completed custom_pk test refactoring.

--- tests/modeltests/custom_pk/tests.py
+++ tests/modeltests/custom_pk/tests.py
@@ -8,20 +8,15 @@ from .models import Employee, Business, Bar, Foo
 
 
 class CustomPKTests(TestCase):
-    def test_custom_pk(self):
-        dan = Employee.objects.create(
+    def setUp(self):
+        self.dan = Employee.objects.create(
             employee_code=123, first_name="Dan", last_name="Jones"
         )
-        self.assertQuerysetEqual(
-            Employee.objects.all(), [
-                "Dan Jones",
-            ],
-            unicode
-        )
-
-        fran = Employee.objects.create(
+        self.fran = Employee.objects.create(
             employee_code=456, first_name="Fran", last_name="Bones"
         )
+
+    def test_objects_with_custom_pk_created_successfully(self):
         self.assertQuerysetEqual(
             Employee.objects.all(), [
                 "Fran Bones",
@@ -30,29 +25,37 @@ class CustomPKTests(TestCase):
             unicode
         )
 
-        self.assertEqual(Employee.objects.get(pk=123), dan)
-        self.assertEqual(Employee.objects.get(pk=456), fran)
+    def test_pk_param_to_get_associated_correctly(self):
+        self.assertEqual(Employee.objects.get(pk=123), self.dan)
+        self.assertEqual(Employee.objects.get(pk=456), self.fran)
 
+    def test_exception_raised_if_employee_non_existent(self):
         self.assertRaises(Employee.DoesNotExist,
             lambda: Employee.objects.get(pk=42)
         )
 
+    def test_get_by_explicit_primary_key(self):
         # Use the name of the primary key, rather than pk.
-        self.assertEqual(Employee.objects.get(employee_code=123), dan)
+        self.assertEqual(Employee.objects.get(employee_code=123), self.dan)
+
+    def test_filter_with_pk(self):
         # pk can be used as a substitute for the primary key.
         self.assertQuerysetEqual(
             Employee.objects.filter(pk__in=[123, 456]), [
                 "Fran Bones",
                 "Dan Jones",
-            ],
+                ],
             unicode
         )
+
+    def test_get_with_pk_param(self):
         # The primary key can be accessed via the pk property on the model.
         e = Employee.objects.get(pk=123)
         self.assertEqual(e.pk, 123)
         # Or we can use the real attribute name for the primary key:
         self.assertEqual(e.employee_code, 123)
 
+    def test_modify_existing_object(self):
         # Fran got married and changed her last name.
         fran = Employee.objects.get(pk=456)
         fran.last_name = "Jones"
@@ -62,75 +65,94 @@ class CustomPKTests(TestCase):
             Employee.objects.filter(last_name="Jones"), [
                 "Dan Jones",
                 "Fran Jones",
-            ],
+                ],
             unicode
         )
 
-        emps = Employee.objects.in_bulk([123, 456])
-        self.assertEqual(emps[123], dan)
 
-        b = Business.objects.create(name="Sears")
-        b.employees.add(dan, fran)
+class CustomPKRelationshipTests(TestCase):
+    def setUp(self):
+        self.dan = Employee.objects.create(
+            employee_code=123, first_name="Dan", last_name="Jones"
+        )
+        self.fran = Employee.objects.create(
+            employee_code=456, first_name="Fran", last_name="Bones"
+        )
+        self.b = Business.objects.create(name="Sears")
+        self.b.employees.add(self.dan, self.fran)
+
+    def test_m2m_with_custom_pk(self):
         self.assertQuerysetEqual(
-            b.employees.all(), [
+            self.b.employees.all(), [
+                "Fran Bones",
                 "Dan Jones",
-                "Fran Jones",
-            ],
+                ],
             unicode
         )
+
+    def test_reverse_m2m_with_custom_pk(self):
         self.assertQuerysetEqual(
-            fran.business_set.all(), [
+            self.fran.business_set.all(), [
                 "Sears",
-            ],
+                ],
             lambda b: b.name
         )
 
+    def test_in_bulk(self):
+        emps = Employee.objects.in_bulk([123, 456])
+        self.assertEqual(emps[123], self.dan)
         self.assertEqual(Business.objects.in_bulk(["Sears"]), {
-            "Sears": b,
+            "Sears": self.b,
         })
 
+    def test_filter_with_custom_key_and_custom_type(self):
         self.assertQuerysetEqual(
             Business.objects.filter(name="Sears"), [
                 "Sears"
             ],
             lambda b: b.name
         )
+
+    def test_filter_with_pk_and_custom_type(self):
         self.assertQuerysetEqual(
             Business.objects.filter(pk="Sears"), [
                 "Sears",
-            ],
+                ],
             lambda b: b.name
         )
 
+    def test_filter_with_related_custom_key_value(self):
         # Queries across tables, involving primary key
         self.assertQuerysetEqual(
             Employee.objects.filter(business__name="Sears"), [
+                "Fran Bones",
                 "Dan Jones",
-                "Fran Jones",
-            ],
+                ],
             unicode,
         )
-        self.assertQuerysetEqual(
-            Employee.objects.filter(business__pk="Sears"), [
-                "Dan Jones",
-                "Fran Jones",
-            ],
-            unicode,
-        )
-
         self.assertQuerysetEqual(
             Business.objects.filter(employees__employee_code=123), [
                 "Sears",
-            ],
+                ],
             lambda b: b.name
+        )
+
+    def test_filter_with_related_pk_value(self):
+        self.assertQuerysetEqual(
+            Employee.objects.filter(business__pk="Sears"), [
+                "Fran Bones",
+                "Dan Jones",
+                ],
+            unicode,
         )
         self.assertQuerysetEqual(
             Business.objects.filter(employees__pk=123), [
                 "Sears",
-            ],
+                ],
             lambda b: b.name,
         )
 
+    def test_filter_with_related_custom_key_comparison(self):
         self.assertQuerysetEqual(
             Business.objects.filter(employees__first_name__startswith="Fran"), [
                 "Sears",
@@ -145,14 +167,9 @@ class CustomPKTests(TestCase):
     def test_unique_pk(self):
         # The primary key must also obviously be unique, so trying to create a
         # new object with the same primary key will fail.
-        e = Employee.objects.create(
-            employee_code=123, first_name="Frank", last_name="Jones"
-        )
-        sid = transaction.savepoint()
         self.assertRaises(IntegrityError,
             Employee.objects.create, employee_code=123, first_name="Fred", last_name="Jones"
         )
-        transaction.savepoint_rollback(sid)
 
     def test_custom_field_pk(self):
         # Regression for #10785 -- Custom fields can be used for primary keys.
