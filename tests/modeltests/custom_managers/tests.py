de9d61f1366fec2b1f3a36cb7b2f7c483245bd9f
Mark Smith <mark.smith@practicalpoetry.co.uk>
2012-07-08 13:27:13 +0200

    Completed custom_columns refactoring.

--- tests/modeltests/custom_managers/tests.py
+++ tests/modeltests/custom_managers/tests.py
@@ -6,19 +6,9 @@ from .models import Person, Book, Car, PersonManager, PublishedBookManager
 
 
 class CustomManagerTests(TestCase):
-    def test_manager(self):
+    def setUp(self):
         p1 = Person.objects.create(first_name="Bugs", last_name="Bunny", fun=True)
         p2 = Person.objects.create(first_name="Droopy", last_name="Dog", fun=False)
-
-        self.assertQuerysetEqual(
-            Person.objects.get_fun_people(), [
-                "Bugs Bunny"
-            ],
-            unicode
-        )
-        # The RelatedManager used on the 'books' descriptor extends the default
-        # manager
-        self.assertTrue(isinstance(p2.books, PublishedBookManager))
 
         b1 = Book.published_objects.create(
             title="How to program", author="Rodney Dangerfield", is_published=True
@@ -26,25 +16,41 @@ class CustomManagerTests(TestCase):
         b2 = Book.published_objects.create(
             title="How to be smart", author="Albert Einstein", is_published=False
         )
+        c1 = Car.cars.create(name="Corvette", mileage=21, top_speed=180)
+        c2 = Car.cars.create(name="Neon", mileage=31, top_speed=100)
 
+    def test_custom_manager_is_correct_type(self):
+        # The RelatedManager used on the 'books' descriptor extends the default
+        # manager
+        self.assertTrue(isinstance(p2.books, PublishedBookManager))
+
+    def test_custom_manager_method(self):
+        self.assertQuerysetEqual(
+            Person.objects.get_fun_people(), [
+                "Bugs Bunny"
+            ],
+            unicode
+        )
+
+    def test_default_manager_doesnt_exist_if_custom_manager_is_provided(self):
         # The default manager, "objects", doesn't exist, because a custom one
         # was provided.
         self.assertRaises(AttributeError, lambda: Book.objects)
 
-        # The RelatedManager used on the 'authors' descriptor extends the
+    def test_related_manager_extends_default_manager(self):
+        The RelatedManager used on the 'authors' descriptor extends the
         # default manager
         self.assertTrue(isinstance(b2.authors, PersonManager))
 
+    def test_assert_custom_manager_all_method_called_correctly(self):
         self.assertQuerysetEqual(
             Book.published_objects.all(), [
                 "How to program",
-            ],
+                ],
             lambda b: b.title
         )
 
-        c1 = Car.cars.create(name="Corvette", mileage=21, top_speed=180)
-        c2 = Car.cars.create(name="Neon", mileage=31, top_speed=100)
-
+    def test_default_manager_correct(self):
         self.assertQuerysetEqual(
             Car.cars.order_by("name"), [
                 "Corvette",
@@ -53,6 +59,7 @@ class CustomManagerTests(TestCase):
             lambda c: c.name
         )
 
+    def test_custom_manager_correct(self):
         self.assertQuerysetEqual(
             Car.fast_cars.all(), [
                 "Corvette",
@@ -60,6 +67,7 @@ class CustomManagerTests(TestCase):
             lambda c: c.name
         )
 
+    def test_underscore_default_manager_attribute_correct(self):
         # Each model class gets a "_default_manager" attribute, which is a
         # reference to the first manager defined in the class. In this case,
         # it's "cars".
